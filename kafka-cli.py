e2852004d9ff5eadaa6353459a3a7d00aaf5f05c
rezaaminimajd <reza.amini.majd1379@gmail.com>
2021-03-07 18:55:09 +0330

    Project skeleton

---
+++ kafka-cli.py
@@ -0,0 +1,32 @@
+import enum
+from compiler import Compiler
+from kafka import KafkaConsumer
+import json
+import time
+
+
+class Topics(enum.Enum):
+    STORE_CODE = "store-code"
+
+
+maximum_try_count = 10
+
+consumer = KafkaConsumer(
+    Topics.STORE_CODE.value,
+    bootstrap_servers='185.97.118.190:9092',
+    group_id='compiler-test',
+    enable_auto_commit=False,
+)
+
+for message in consumer:
+    data = json.loads(message.value.decode("utf-8"))
+    result = Compiler.compile(code_id=data['code_id'], language=data['language'])
+    if result:
+        consumer.commit()
+    else:
+        try_count = 0
+        while not Compiler.compile(code_id=data['code_id'],
+                                   language=data['language']) and try_count < maximum_try_count:
+            try_count += 1
+            time.sleep(try_count)
+        consumer.commit()
