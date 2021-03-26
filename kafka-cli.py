7f36ba4443b73a645c365bcfd1d56d80a1627e83
rezaaminimajd <reza.amini.majd1379@gmail.com>
2021-03-26 18:29:39 +0430

    last edit

--- kafka-cli.py
+++ kafka-cli.py
@@ -13,20 +13,20 @@ maximum_try_count = 10
 
 consumer = KafkaConsumer(
     Topics.STORE_CODE.value,
-    bootstrap_servers='185.97.118.190:9092',
-    group_id='compiler-test',
-    enable_auto_commit=False,
+    bootstrap_servers='185.204.197.207:9092',
+    group_id=None,
+    auto_offset_reset='earliest'
 )
 
 for message in consumer:
     data = json.loads(message.value.decode("utf-8"))
     result = Compiler.compile(code_id=data['code_id'], language=data['language'])
-    if result:
-        consumer.commit()
-    else:
-        try_count = 0
-        while not Compiler.compile(code_id=data['code_id'],
-                                   language=data['language']) and try_count < maximum_try_count:
-            try_count += 1
-            time.sleep(try_count)
-        consumer.commit()
+    # if result:
+    #     consumer.commit()
+    # else:
+    #     try_count = 0
+    #     while not Compiler.compile(code_id=data['code_id'],
+    #                                language=data['language']) and try_count < maximum_try_count:
+    #         try_count += 1
+    #         time.sleep(try_count)
+    #     consumer.commit()
